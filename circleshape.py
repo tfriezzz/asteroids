cd87f25cc07cce6991f95e1719900f901a302877
tfriezz <roswall.tim@gmail.com>
2024-09-01 17:10:45 +0200

    add player

--- circleshape.py
+++ circleshape.py
@@ -1,9 +1,7 @@
 import pygame
 
-# Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
@@ -14,9 +12,7 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius = radius
 
     def draw(self, screen):
-        # sub-classes must override
         pass
 
     def update(self, dt):
-        # sub-classes must override
         pass
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pygame
 from constants import*
+from player import*
 
 clock = pygame.time.Clock()
 dt = 0
+
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
+player = Player(x, y, PLAYER_RADIUS)
+
 
 def main():
     pygame.init()
@@ -12,9 +18,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
         dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
